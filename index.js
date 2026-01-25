37fed3e0c56e94be679f2e193c6e05168334230b
MoniqueJoeVonnie <jovonnie.mb@gmail.com>
2026-01-25 16:07:50 -0500

    save

---
+++ index.js
@@ -0,0 +1,25 @@
+// 
+//
+// 
+
+function contact(event) {
+    event.preventDefault();
+    //    emailjs
+    //         .sendForm(
+    //             'service_rw18ppl',
+    //             'template_bbgffga',
+    //         event.target,
+    //         'uGyalPMFUSbqaTqPS'
+    //     ).then(() => {
+    //         console.log('this worked1')
+    //     })
+    const loading = document.querySelector(' .modal__overlay--loading')
+        const sucess = document.querySelector(' .modal__overlay--success')
+    loading.classList += " modal__overlay--visible";
+    setTimeout(() => {
+     loading.classList.remove("modal__overlay--visible");
+     success.classList += " modal__overlay--visible"
+     console.log('it worked 1')
+    }, 100);
+
+}
